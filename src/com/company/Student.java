9a253e8eefd8d8f983e4a7b85acd16cf53ebf6a5
Tsvetan <408366@student.fontys.nl>
2021-05-06 18:24:24 +0200

    creation of n students, showing their info, paying tuitions

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Student {
@@ -8,10 +10,11 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentID;
-    private String courses;
-    private int tuitionBalance;
+    private List<String> courses = new ArrayList<>();
+    private int tuitionBalance = 0;
     private static int costToEnroll = 600;
     private static int id = 1000;
+
 
 
     // constructor to enter the name and the year
@@ -27,9 +30,6 @@ public class Student {
         this.gradeYear = in.nextInt();
 
         setStudentId();
-
-        System.out.println(this.firstName + " " + this.lastName + " " + this.gradeYear + " " + this.studentID);
-
     }
 
     //generate an ID
@@ -39,11 +39,53 @@ public class Student {
     }
 
     //enroll in courses
+    public void enroll(){
+        // Get inside a loop. user hits 0
+        while(true){
+            System.out.println("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses.add(course);
+                tuitionBalance += costToEnroll;
+            }
+            else{break;}
+        }
+    }
 
     //view balance
+    public void viewBalance(){
+        System.out.println("Your balance is: " + tuitionBalance);
+    }
 
     //pay tuition
+    public void payTuition(){
+        viewBalance();
+        System.out.println("Enter your payment: ");
+        Scanner in = new Scanner(System.in);
+        boolean isValid = true;
+        int payment = in.nextInt();
+        while(isValid){
+            if (payment > tuitionBalance || payment < 0){
+                System.out.println("Please enter a valid amount!");
+                payment = in.nextInt();
+            }
+            else{
+                isValid = false;
+            }
+        }
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of " + payment);
+        viewBalance();
+    }
 
     //Show information
+    public String showInfo(){
+        return "Name: " + this.firstName + " " + this.lastName +
+                "\nGrade Level: " + this.gradeYear +
+                "\nStudent ID: " + this.studentID +
+                "\nCourses Enrolled: " + this.courses +
+                "\nBalance: " + this.tuitionBalance;
+    }
 
 }
